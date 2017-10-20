1907e7795fb989c35a5ca2db169c68781a90cbe4
CB Fay <31521775+CBFay@users.noreply.github.com>
2017-10-19 22:21:54 -0400

    Useful Keybindings for Notepad++

---
+++ Java 0001 - npp/npp.java
@@ -0,0 +1,34 @@
+// created 09.02.2017
+
+public class npp {
+	
+	/* Keyboard Shortcuts for Notepad++
+	
+	(CTRL + LEFT/RIGHT) 		Jump words
+	(CTRL + SHIFT + LEFT/RIGHT) 	Highlighter scroll
+	(CTRL + SHIFT + DELETE) 	Delete the rest of the current line
+	(CTRL + ALT + SHIFT + ENTER) 	Insert blank line below
+	(CTRL + ALT + ENTER) 		Insert blank line above
+	(CTRL + Q) 			Comment out a selection
+	(CTRL + W) 			Close current document
+	(CTRL + SHIFT + S)		Save all open documents
+	(CTRL + TAB)			Cycle through open documents
+	(CTRL + D)			Duplicate current line
+	(CTRL + T)			Swap current line with previous line
+	(CTRL + L)			Delete current line
+	(CTRL + SHIFT + DOWN)		Move current line down
+	(CTRL + SHIFT + UP)		Move current line up
+	(ALT + LEFT CLICK)		Column mode select
+	(CTRL + U)			Convert to lowercase
+	(CTRL + SHIFT + U)		Convert to uppercase
+	(CTRL + F)			Find/Replace
+	(CTRL + B)			Go to corresponding brace
+	(F3)				Find next				
+	(CTRL + B)			Go to corresponding brace
+	(CTRL + F2)			Set/Unset bookmark
+	(F2)				Next bookmark
+	(CTRL + (+))			Font size up
+	(CTRL + (-))			Font size down
+	
+	*/
+}
