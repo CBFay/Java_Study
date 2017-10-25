ebaa9ca18fad6b86da7a4dabeab6204419ee8860
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-25 11:56:29 -0400

    Creating a basic class

---
+++ Java 0036 - Tuna/Tuna.java
@@ -0,0 +1,12 @@
+/*
+  - From "thenewboston"'s youtube series on Java
+  - Takes a string and says hello
+  - Combine multiple classes to accomplish goals
+  - Created 09.15.2017 by CB Fay
+*/
+
+public class Tuna {
+  public void simpleMessage(String name) {
+    System.out.println("Hello " + name);
+  }
+}
