ccfd89ec0689e3ac5e99f25c261eca746b06b536
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-10-25 12:44:40 -0400

    Learning the nuances of breaks with labels

---
+++ Java 0043 - Break5/Break5.java
@@ -0,0 +1,24 @@
+/*	
+	-	From pg 91 of "Java: A Beginner's Guide - Sixth Edition" 
+	-	uses a break with a label
+	-	look closely, break behavior isn't exactly what seems intuitive
+	-	Created 09.18.2017 by CB Fay
+*/
+
+class Break5 {
+	public static void main(String[] args) {
+		
+done:
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				for(int k=0; k<10; k++) {
+					System.out.println(k + " ");
+				if(k==5) break done; // jump to done
+				}
+				System.out.println("After k loop"); // won't execute	
+			}
+			System.out.println("After j loop"); // won't execute	
+		}
+		System.out.println("After i loop");
+	}
+}
