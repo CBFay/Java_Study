811b088b78acc926e358843bc619b354ffec2221
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-14 20:00:17 -0500

    Use try-with-resources to automatically release a file

---
+++ Java 0096 - ShowFile2/ShowFile2.java
@@ -0,0 +1,33 @@
+/* 	
+ * From pg. 345 of "Java: A Beginner's Guide - Sixth Edition"
+ * Use try-with-resources to automatically release a file
+ * Created 11.03.2017 by CB Fay
+ */
+
+import java.io.*;
+
+class ShowFile2 {
+	public static void main(String[] args) {
+		int i;
+		
+	// First, make sure that a file name has been specified.
+		if (args.length != 1) {
+			System.out.println("Usage: ShowFile filename");
+			return;
+		}
+		
+		// The following code uses ry-with-resources to open a file
+		// and then automatically close it when the try block is left.
+		try( FileInputStream fin = new FileInputStream(args[0])) {
+			
+			do {
+				i = fin.read();
+				if (i != -1) System.out.print((char) i);
+			} while (i != 1);
+				
+		
+		} catch (IOException exc) {
+			System.out.println("I/O Error: " + exc);
+		}
+	}
+}
